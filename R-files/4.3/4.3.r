386d6fe7f4b31d31ed32ac674aeefbbc5cb7be96
nuno99costa <nunopf99@gmail.com>
2021-04-30 01:42:26 +0100

    4.3.a - added kendall correlation (awaiting professor) 4.3.b - missing pearson pre-assumptions

--- R-files/4.3/4.3.r
+++ R-files/4.3/4.3.r
@@ -5,54 +5,65 @@ library(ggplot2)
 library(scales)
 library(reshape2)
 library(nortest)
-
+library(ggpubr)
+library(car)
+library(PMCMRplus)
 covid_data <- read_csv("../owid-covid-data.csv")
 
 
-#4.3 Correlação
+###4.3 Correlação
 
 #lista de datas possíveis
 date_list <- seq(as.Date("2021-01-01"), as.Date("2021-12-31"), by = "day")
 
-#4.3.a
+##4.3.a
 
 #obter dados relativos ao problema (e remover NAs)
 europe <- subset(covid_data, covid_data$continent == 'Europe' & covid_data$population>10000000 & date %in% date_list, select=c(location, reproduction_rate, population_density))
 clean_europe <- na.omit(europe)
 
 #filtrar dados de modo a obter a máxima taxa de transmissibilidade por país
-countries_highest_rate <- group_by(clean_europe, location) %>% 
+highest_rp_pd <- group_by(clean_europe, location) %>% 
   filter(reproduction_rate == max(reproduction_rate)) %>% 
   filter(1:n() == 1)
 
-## devemos remover outliers??
+#testamos a normalidade das amostras (Shapiro)
+plot_pd <- ggqqplot(highest_rp_pd$population_density)
+png(filename="4.3.a_qqplot_pd.png")
+plot(plot_pd)
+dev.off()
 
-#shapiro ou anderson darling
-ggqqplot(countries_highest_rate$reproduction_rate)
-ggqqplot(countries_highest_rate$population_density)
-shapiro.test(countries_highest_rate$reproduction_rate)
-shapiro.test(countries_highest_rate$population_density)
-ad.test(countries_highest_rate$reproduction_rate)
-ad.test(countries_highest_rate$population_density)
+plot_rd <- ggqqplot(highest_rp_pd$reproduction_rate)
+png(filename="4.3.a_qqplot_rd.png")
+plot(plot_rd)
+dev.off()
 
-cor.test (c(countries_highest_rate$population_density) ,c(countries_highest_rate$reproduction_rate) , alternative ="two.sided", method ="pearson")
+shapiro.test(highest_rp_pd$reproduction_rate)
+shapiro.test(highest_rp_pd$population_density)
 
-#4.3.b
+#como a amostra em análise é pequena e uma das variavéis não é normalmente distríbuida, usamos test de Kendall
+cor(c(highest_rp_pd$population_density) ,c(highest_rp_pd$reproduction_rate) , method ="kendall")
+
+#coeficiente = -0.08
+#assim, assumimos não existe nenhuma correlação significativa entre as duas variáveis
+
+##4.3.b
 
 #obter dados
 total_deaths_per_million_per_country <- subset(covid_data, covid_data$continent == 'Europe' & covid_data$population>10000000 & date %in% date_list, select=c(location,total_deaths_per_million, aged_65_older))
 
 #filtrar dados de modo a obter os máximos por país
-countries_highest_rate <- group_by(total_deaths_per_million_per_country, location) %>% 
+highest_td_a65 <- group_by(total_deaths_per_million_per_country, location) %>% 
   filter(total_deaths_per_million == max(total_deaths_per_million)) %>% 
   filter(1:n() == 1)
 
-#shapiro ou anderson darling
-ggqqplot(countries_highest_rate$total_deaths_per_million)
-ggqqplot(countries_highest_rate$aged_65_older)
-shapiro.test(countries_highest_rate$total_deaths_per_million)
-shapiro.test(countries_highest_rate$aged_65_older)
+#verificar 
+plot(highest_td_a65$total_deaths_per_million,highest_td_a65$aged_65_older)
+shapiro.test(highest_td_a65$total_deaths_per_million)
+shapiro.test(highest_td_a65$aged_65_older)
 
-#dados são normais
+#dados seguem distribuição normal
+#
+
 #teste de correlação
-cor.test (c(countries_highest_rate$total_deaths_per_million) ,c(countries_highest_rate$aged_65_older), method ="pearson")
+cor.test(c(highest_td_a65$total_deaths_per_million) ,c(highest_td_a65$aged_65_older), method ="pearson")
